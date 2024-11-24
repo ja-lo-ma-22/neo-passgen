6e65b2092b72b9f735e0a1ceef0a55d8159be4c2
Jakob Logan Marter <ja-lo-ma-22@protonmail.com>
2024-11-24 14:02:28 -0800

    Added unfinished processing for command-line arguments. Is currently only for testing. Needs more work.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 // converts a seed string into a hashed password string in base94.
 //
 // summary:
-// seed string -> sha512sum -> base94cli -> password
+// seed string -> sha512 -> base94 -> password
 
-// is used to collect a seed String from the user.
+// collects a seed String from the user
 use std::io;
 
-// SHA512sum hashes binary data
+// reads command-line arguments from the user
+use std::env;
+
+// SHA512 hashes binary data
 use sha2::{Sha512, Digest};
 
-// Base94 converts data to base94
+// Base94 converts data to base94 String
 use base94::encode;
 
 fn main() {
 
     // a mutable String that the seed is stored in.
     let mut seed = String::new();
+
+    // collects command-line arguments into a vector
+    let args: Vec<String> = env::args().collect();
+
+    // values for later use in the program
+    let mut testing: bool = false;
+    let mut program_name = String::new();
+
+    // iterates on the vector of command line arguments
+    for argument in args {
+        match argument.as_str() {
+            "testing" => { testing = true; }
+            _ => {
+                if program_name.is_empty() {
+                    program_name = String::from(argument);
+                } else {
+                    panic!("{} is not a valid argument.", argument);
+                }
+            }
+        }
+    }
 
     // collects the String data from the user and processes potential errors.
     io::stdin()
@@ -28,7 +52,7 @@ fn main() {
     // inputs seed data
     hasher.update(& seed);
 
-    // processes the hash and consumes the hasher object
+    // processes the hash and consumes the hasher object.
     let seed = hasher.finalize();
 
     // encodes the hashed_seed in base94
